b66325b55988999420aeade928e93612962243da
Charles <cobas050@uottawa.ca>
2021-07-22 15:09:07 -0400

    another commit

--- src/database.ts
+++ src/database.ts
@@ -6,17 +6,17 @@ dotenv.config();
 
 const {
     POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD
+    Database,
+    Username,
+    password
 } = process.env
 
 
 const client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
+    database: Database,
+    user: Username,
+    password: password
 });
 
 
